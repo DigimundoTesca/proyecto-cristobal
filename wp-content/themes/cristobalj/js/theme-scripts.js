1de91dc4b0a42277308b4b074452a9d6236b8be3
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2017-08-19 22:41:29 -0500

    Addes french and italian 'suscribe' translation

--- wp-content/themes/cristobalj/js/theme-scripts.js
+++ wp-content/themes/cristobalj/js/theme-scripts.js
@@ -1,16 +1,15 @@
 
 jQuery(document).ready(function(){
-  alert(window.location.pathname);
 
   // Show and hidde mobile menu
   if (window.location.pathname == '/Cristobal/') {
     document.getElementById('es_txt_button_pg').value = 'suscribirse';
   }
   else if (window.location.pathname == '/Cristobal/it/') {
-    document.getElementById('es_txt_button_pg').value = 'italiano';
+    document.getElementById('es_txt_button_pg').value = 'abbonarsi';
   }
   else if (window.location.pathname == '/Cristobal/fr/') {
-    document.getElementById('es_txt_button_pg').value = 'frances';
+    document.getElementById('es_txt_button_pg').value = "S'abonner";
   }
   
 });
