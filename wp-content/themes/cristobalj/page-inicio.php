a830351ce1ae48151bef33a555b0ae4bda959185
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2017-08-19 21:16:56 -0500

    Moddifying styles for banner

--- wp-content/themes/cristobalj/page-inicio.php
+++ wp-content/themes/cristobalj/page-inicio.php
@@ -8,15 +8,25 @@ get_header(); ?>
   <main id="main" class="site-main">
     <div id="banner" class="bannermobile">
     </div>
-      <div class="bannercontainer">
-        <div class="bannertext"></div>
+      <div class="mobilecontainer">
+        <div class="bannertext">
+          <h2 class="bannertitle">
+            <?php printf(__('Welcome!','cristobalj')) ?>
+          </h2>
+          <p class="quote"><?php printf(__('"Here you can put a phrase from Cristobal that welcomes people"','cristobalj')) ?></p>
+        </div>
         <div class="newscontainer">
           <?php es_subbox( $namefield = "NO", $desc = "", $group = "" ); ?>
         </div>
       </div>
     <div id="banner1" class="bannerbig">
       <div class="bannercontainer">
-        <div class="bannertext"></div>
+        <div class="bannertext">
+          <h2 class="bannertitle">
+            <?php printf(__('Welcome!','cristobalj')) ?>
+          </h2>
+          <p class="quote"><?php printf(__('"Here you can put a phrase from Cristobal that welcomes people"','cristobalj')) ?></p>
+        </div>
         <div class="newscontainer">
           <?php es_subbox( $namefield = "NO", $desc = "", $group = "" ); ?>
         </div>
