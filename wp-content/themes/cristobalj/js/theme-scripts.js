be16a5abfd85d1dba262ecf532b6e6723a016a91
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2017-08-19 22:53:24 -0500

    Moddified funtcion to translate submit button to use class name

--- wp-content/themes/cristobalj/js/theme-scripts.js
+++ wp-content/themes/cristobalj/js/theme-scripts.js
@@ -2,14 +2,18 @@
 jQuery(document).ready(function(){
 
   // Show and hidde mobile menu
-  if (window.location.pathname == '/Cristobal/') {
-    document.getElementById('es_txt_button_pg').value = 'suscribirse';
-  }
-  else if (window.location.pathname == '/Cristobal/it/') {
-    document.getElementById('es_txt_button_pg').value = 'abbonarsi';
-  }
-  else if (window.location.pathname == '/Cristobal/fr/') {
-    document.getElementById('es_txt_button_pg').value = "S'abonner";
+  let buttonsubmit = document.getElementsByClassName('es_textbox_button es_submit_button');
+  let i = 0;
+  for (; i < buttonsubmit.length; i++) {
+    if (window.location.pathname == '/Cristobal/') {
+      document.getElementsByClassName('es_textbox_button es_submit_button')[0].value = 'suscribirse';
+    }
+    else if (window.location.pathname == '/Cristobal/it/') {
+      document.getElementsByClassName('es_textbox_button es_submit_button')[0].value = 'abbonarsi';
+    }
+    else if (window.location.pathname == '/Cristobal/fr/') {
+      document.getElementsByClassName('es_textbox_button es_submit_button')[0].value = "S'abonner";
+    }
   }
   
 });
