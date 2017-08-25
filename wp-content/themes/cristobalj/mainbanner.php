0005b4249056533ba08e2ee18b0f94785ac216d8
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2017-08-24 22:09:09 -0500

    Working with banner that shows metamundo, book and mindhack blocks

---
+++ wp-content/themes/cristobalj/mainbanner.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Template Name: mainbanner
+*/
+?>
+<div id="mainbannercontainer">
+  <div class="mainbannercontent"></div>
+  <div class="mainbannercontent"></div>
+  <div class="mainbannercontent"></div>
+</div>
--- wp-content/themes/cristobalj/page-inicio.php
+++ wp-content/themes/cristobalj/page-inicio.php
@@ -8,17 +8,17 @@ get_header(); ?>
   <main id="main" class="site-main">
     <div id="banner" class="bannermobile">
     </div>
-      <div class="mobilecontainer">
-        <div class="bannertext">
-          <h2 class="bannertitle">
-            <?php printf(__('Welcome!','cristobalj')) ?>
-          </h2>
-          <p class="quote"><?php printf(__('"Here you can put a phrase from Cristobal that welcomes people"','cristobalj')) ?></p>
-        </div>
-        <div class="newscontainer">
-          <?php es_subbox( $namefield = "NO", $desc = "", $group = "" ); ?>
-        </div>
+    <div class="mobilecontainer">
+      <div class="bannertext">
+        <h2 class="bannertitle">
+          <?php printf(__('Welcome!','cristobalj')) ?>
+        </h2>
+        <p class="quote"><?php printf(__('"Here you can put a phrase from Cristobal that welcomes people"','cristobalj')) ?></p>
       </div>
+      <div class="newscontainer">
+        <?php es_subbox( $namefield = "NO", $desc = "", $group = "" ); ?>
+      </div>
+    </div>
     <div id="banner1" class="bannerbig">
       <div class="bannercontainer">
         <div class="bannertext">
@@ -34,9 +34,18 @@ get_header(); ?>
       </div>
     </div>
 
+
+    <div class="mainpage" id="mainpage">
+      <?php get_template_part( 'mainbanner' );  ?>
+      <div id="datesContainer">
+      hola
+      </div>
+    </div>
+
+
   </main><!-- #main -->
 </div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
+?>
