33b6fcb49ca2b41ab58e01eb4482641df3d5c2f8
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2017-08-24 23:38:21 -0500

    Added index changes to french and italian pages

--- wp-content/themes/cristobalj/page-iniziazione.php
+++ wp-content/themes/cristobalj/page-iniziazione.php
@@ -8,17 +8,17 @@ get_header(); ?>
   <main id="main" class="site-main">
     <div id="banner" class="bannermobile">
     </div>
-      <div class="mobilecontainer">
-        <div class="bannertext">
-          <h2 class="bannertitle">
-            <?php printf(__('Welcome!','cristobalj')) ?>
-          </h2>
-          <p class="quote"><?php printf(__('"Here you can put a phrase from Cristobal that welcomes people"','cristobalj')) ?></p>
-        </div>
-        <div class="newscontainer">
-          <?php es_subbox( $namefield = "NO", $desc = "", $group = "" ); ?>
-        </div>
+    <div class="mobilecontainer">
+      <div class="bannertext">
+        <h2 class="bannertitle">
+          <?php printf(__('Welcome!','cristobalj')) ?>
+        </h2>
+        <p class="quote"><?php printf(__('"Here you can put a phrase from Cristobal that welcomes people"','cristobalj')) ?></p>
       </div>
+      <div class="newscontainer">
+        <?php es_subbox( $namefield = "NO", $desc = "", $group = "" ); ?>
+      </div>
+    </div>
     <div id="banner1" class="bannerbig">
       <div class="bannercontainer">
         <div class="bannertext">
@@ -26,16 +26,26 @@ get_header(); ?>
             <?php printf(__('Welcome!','cristobalj')) ?>
           </h2>
           <p class="quote"><?php printf(__('"Here you can put a phrase from Cristobal that welcomes people"','cristobalj')) ?></p>
+          <p class="name">Cristóbal Jodorowsky</p>
         </div>
         <div class="newscontainer">
-          <?php es_subbox( $namefield = "NO", $desc = "", $group = "" ); ?>
+          <?php es_subbox( $namefield = "NO", $desc = "", $group = "Maingroup" ); ?>
         </div>
       </div>
     </div>
+
+
+    <div class="mainpage" id="mainpage">
+      <?php get_template_part( 'mainbanner' );  ?>
+      <div id="datesContainer">
+      hola
+      </div>
+    </div>
+
 
   </main><!-- #main -->
 </div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
+?>
