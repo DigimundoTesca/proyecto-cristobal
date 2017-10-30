e3a30874d262673e604bc175a70910f24d122a26
Itzli <itzli2000@msn.com>
2017-10-30 11:37:05 -0600

    Added styles to book download page

--- wp-content/themes/cristobalj/page-bookdownload.php
+++ wp-content/themes/cristobalj/page-bookdownload.php
@@ -5,9 +5,19 @@
 get_header(); ?>
 
 <div id="primary" class="content-area">
-  <main id="main" class="site-main d-flex justify-content-center">
-    <?php echo do_shortcode('[contact-form-7 title="Descarga de libro"]'); ?>
-    <a download href="<?php echo get_stylesheet_directory_uri(); ?>/El_collar_del_tigre.pdf" id="downloadThis" class="d-none" title="Libro Tigre">Download</a>
+  <main id="main" class="site-main d-flex flex-column align-items-center bookdownloadContainer">
+    <h2 class="bannertitle py-5">
+      <?php printf(__('Download "El collar del tigre"!','cristobalj')) ?>
+    </h2>
+    <div class="row d-flex flex-wrap">
+      <div class="col-sm-12 col-lg-6 downloadForm">
+        <?php echo do_shortcode('[contact-form-7 title="Descarga de libro"]'); ?>
+        <a download href="<?php echo get_stylesheet_directory_uri(); ?>/El_collar_del_tigre.pdf" id="downloadThis" class="d-none" title="Libro Tigre">Download</a>
+      </div>
+      <div class="col-lg-6 d-flex justify-content-center align-items-center downloadImage">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/books/Tigre1.jpg" alt="book image">
+      </div>
+    </div>
   </main><!-- #main -->
 </div><!-- #primary -->
 
