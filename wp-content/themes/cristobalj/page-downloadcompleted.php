cc406fd3ddce682033cd30666642e340b569f320
Itzli <itzli2000@msn.com>
2017-11-21 13:50:02 -0600

    refactor: rename page after book download
    Changed from 'thanks' to 'downloadcompleted', modify theme-scripts,
    functions and language translations

---
+++ wp-content/themes/cristobalj/page-downloadcompleted.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template for displaying book download form */
+
+get_template_part( 'header_book' );  ?>
+
+<div id="primary" class="content-area">
+  <main id="main" class="site-main d-flex flex-column align-items-center bookdownloadContainer">
+    <h2 class="bannertitle py-5">
+      <?php printf(__('You have downloaded - "El collar del tigre"','cristobalj')) ?>
+    </h2>
+    <div class="row" style=" height:50vh;">
+      <div class="col-12 d-flex justify-content-center align-items-center">
+        <h3 class="display-3"><?php printf(__('See you soon','cristobalj')) ?></h3>
+      </div>
+    </div>
+  </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php
+get_footer();
+?>
