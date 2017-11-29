5008da7a279750ba4bd990a38c8bfb14e5bbeb48
Itzli <itzli2000@msn.com>
2017-11-29 09:50:23 -0600

    refactor: complete modifications
    On mainbanner and secondarybanner squares

--- wp-content/themes/cristobalj/mainbanner.php
+++ wp-content/themes/cristobalj/mainbanner.php
@@ -7,18 +7,22 @@ Template Name: mainbanner
   <div class="mainbannercontent">
     <a href="<?php echo esc_url( get_permalink( get_page_by_title( 'Metamundo' ) ) ); ?>" title="Metamundo">
       <div class="metmundo">
-      <h3>
-      <?php printf(__('Live the experience!','cristobalj')) ?>
-    </h3>
-    <p class="text-center">
-      <?php printf(__('"Next generations"','cristobalj')) ?>
-    </p>
+        <h3>
+          <?php printf(__('Live the experience!','cristobalj')) ?>
+        </h3>
+        <p class="text-center text-dark h5 my-0">
+          <?php printf(__('"Next generations"','cristobalj')) ?>
+        </p>
+        <p class="text-center text-dark mb-0">
+          <?php printf(__('Mexico febrero 2018','cristobalj')) ?>
+          <?php printf(__('Argentina julio 2018','cristobalj')) ?>
+        </p>
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/metalogonuevo.png" alt="">
       </div>
     </a>
   </div>
   <div class="mainbannercontent">
-    <a data-toggle="modal" data-target="#downloadBook">
+    <a href="<?php echo esc_url( get_permalink( get_page_by_title( 'Bookdownload' ) ) ); ?>" title="Bookdownload">
       <div class="gift">
         <h3>
           <?php printf(__('We give you','cristobalj')) ?>
@@ -31,14 +35,11 @@ Template Name: mainbanner
     </a>
   </div>
   <div class="mainbannercontent">
-    <a href="http://mindhackers.mx" target="_blank" title="Mind Hackers">
+    <a href="#" title="Tarot">
       <div class="mindHack">
         <h3>
-          <?php printf(__('Cristobal','cristobalj')) ?>
-        </h3>
-        <p>
           <?php printf(__('Tarot queries','cristobalj')) ?>
-        </p>
+        </h3>
         <img  src="<?php echo get_stylesheet_directory_uri(); ?>/images/Tarot.jpg"  alt="">
       </div>
     </a>
--- wp-content/themes/cristobalj/secondarybanner.php
+++ wp-content/themes/cristobalj/secondarybanner.php
@@ -10,15 +10,19 @@ Template Name: secondarybanner
         <h3>
           <?php printf(__('Live the experience!','cristobalj')) ?>
         </h3>
-        <p class="text-center">
+        <p class="text-center text-dark h5 my-0">
           <?php printf(__('"Next generations"','cristobalj')) ?>
+        </p>
+        <p class="text-center text-dark mb-0">
+          <?php printf(__('Mexico febrero 2018','cristobalj')) ?>
+          <?php printf(__('Argentina julio 2018','cristobalj')) ?>
         </p>
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/metalogonuevo.png" alt="">
       </div>
     </a>
   </div>
   <div class="mainbannercontent">
-    <a data-toggle="modal" data-target="#downloadBook">
+    <a href="<?php echo esc_url( get_permalink( get_page_by_title( 'Bookdownload' ) ) ); ?>" title="Bookdownload">
       <div class="gift">
         <h3>
           <?php printf(__('We give you','cristobalj')) ?>
@@ -34,11 +38,8 @@ Template Name: secondarybanner
     <a href="http://mindhackers.mx" target="_blank" title="Mind Hackers">
       <div class="mindHack">
         <h3>
-          <?php printf(__('Cristobal','cristobalj')) ?>
-        </h3>
-        <p>
           <?php printf(__('Tarot queries','cristobalj')) ?>
-        </p>
+        </h3>
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Tarot.jpg" alt="">
       </div>
     </a>
