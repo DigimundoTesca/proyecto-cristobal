3701170b7dc39c36ae6750f4a00e4771fdb8121c
carlosmata1802 <charlixd.cm@gmail.com>
2018-03-27 18:12:07 -0600

    Fix error image share facebook.

--- wp-content/themes/cristobalj/header.php
+++ wp-content/themes/cristobalj/header.php
@@ -31,7 +31,31 @@
   <meta name="description" content="<?php bloginfo('description'); ?>" />
   <meta name="keywords" content="Cristobal Jodorowsky, metamundo, psicochamanismo, metagenealogia,agenda" />
   <meta name="author" content="Digimundo Technologies" />
+	<?php
+ $thumb = get_post_meta($post->ID,'_thumbnail_id',false);
+ $thumb = wp_get_attachment_image_src($thumb[0], false);
+ $thumb = $thumb[0];
+ $default_img = get_bloginfo('stylesheet_directory').'/imagenes/imagen_por_defecto.png';
 
+ ?>
+
+<?php if(is_single() || is_page()) { ?>
+ <meta property="og:type" content="article" />
+ <meta property="og:title" content="<?php single_post_title(''); ?>" />
+ <meta property="og:description" content="<?php
+ while(have_posts()):the_post();
+ $out_excerpt = str_replace(array("\r\n", "\r", "\n"), "", get_the_excerpt());
+ echo apply_filters('the_excerpt_rss', $out_excerpt);
+ endwhile; ?>" />
+ <meta property="og:url" content="<?php the_permalink(); ?>"/>
+ <meta property="og:image" content="<?php if ( $thumb[0] == null ) { echo $default_img; } else { echo $thumb; } ?>" />
+<?php  } else { ?>
+ <meta property="og:type" content="article" />
+   <meta property="og:title" content="<?php bloginfo('name'); ?>" />
+ <meta property="og:url" content="<?php bloginfo('url'); ?>"/>
+ <meta property="og:description" content="<?php bloginfo('description'); ?>" />
+    <meta property="og:image" content="<?php  if ( $thumb[0] == null ) { echo $default_img; } else { echo $thumb; } ?>" />
+<?php  }  ?>
 	<?php wp_head(); ?>
 
 </head>
